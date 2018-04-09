b645e2ae46e84ec743c93b236cb764e8fa33a3ec
Fengming Sun <sunfengming@meituan.com>
2018-04-09 15:18:37 +0800

    fix for include

--- main.go
+++ main.go
@@ -3,24 +3,24 @@ package main
 import (
 	"fmt"
 
-	IdGen "./idgen"
+	"sfmblog.cn/idgenEx/idgen"
 )
 
 var channel chan int = make(chan int)
 
-func loop(idGenerator *IdGen.IDGenerator) {
+func loop(idGenerator *idgen.IDGenerator) {
 	for i := 0; i < 100; i++ {
 		id, err := idGenerator.Next("test", 12345678)
 		if err != nil {
 			fmt.Println(err)
 		}
-		fmt.Printf("%v -> %d \n", IdGen.ParseID(id), id)
+		fmt.Printf("%v -> %d \n", idgen.ParseID(id), id)
 	}
 	channel <- 0
 }
 
 func main() {
-	idGenerator := IdGen.NewIDGenerator("119.27.180.231", 6379, "c5809078fa6d652e0b0232d552a9d06d37fe819c")
+	idGenerator := idgen.NewIDGenerator("119.27.180.231", 6379, "c5809078fa6d652e0b0232d552a9d06d37fe819c")
 	go loop(idGenerator)
 	go loop(idGenerator)
 	go loop(idGenerator)
