4d05090d7e68ad1d8be68ab753084a45f03d1770
Fengming Sun <sunfengming@meituan.com>
2018-04-09 11:05:51 +0800

    add for goroutine

--- idgen/idgen.go
+++ idgen/idgen.go
@@ -1,66 +1,77 @@
 package idgen
 
 import (
-    "fmt"
-    "strconv"
-    "errors"
+	"errors"
+	"fmt"
+	"strconv"
 
-    "github.com/go-redis/redis"
+	"github.com/go-redis/redis"
 )
 
 // IDGenerator struct
 type IDGenerator struct {
-    client *redis.Client
-    hash   string
+	client *redis.Client
+	hash   string
 }
 
 // NewIDGenerator construct
 func NewIDGenerator(host string, port int, hash string) *IDGenerator {
-    client := redis.NewClient(&redis.Options{
-        Addr:     fmt.Sprintf("%s:%d", host, port),
-        Password: "",
-        DB:       0,
-    })
+	client := redis.NewClient(&redis.Options{
+		Addr:     fmt.Sprintf("%s:%d", host, port),
+		Password: "",
+		DB:       0,
+	})
 
-    return &IDGenerator{client, hash}
+	return &IDGenerator{client, hash}
 }
 
 // Next func
-func (idGen *IDGenerator) Next(tableName string, shardID int) ( int64, error ) {
-    result, err := idGen.client.EvalSha(idGen.hash, []string{tableName, strconv.Itoa(shardID)}).Result()
-    var paramSlice []int64
+func (idGen *IDGenerator) Next(tableName string, shardID int) (int64, error) {
+	result, err := idGen.client.EvalSha(idGen.hash, []string{tableName, strconv.Itoa(shardID)}).Result()
+	var paramSlice []int64
 
-    if params, ok := result.([]interface{}); ok {
-        for _, param := range params {
-            paramSlice = append(paramSlice, param.(int64))
-        }
-        return buildID(paramSlice...), nil
-    }
+	if params, ok := result.([]interface{}); ok {
+		for _, param := range params {
+			paramSlice = append(paramSlice, param.(int64))
+		}
+		return buildID(paramSlice...), nil
+	}
 
-    if err != nil {
-        return 0, err
-    }
+	if err != nil {
+		return 0, err
+	}
 
-    return 0, errors.New("id generator result parse error")
+	return 0, errors.New("id generator result parse error")
 }
 
 func buildID(args ...int64) int64 {
-    var id int64;
-    for index, arg := range args {
-        switch index {
-        case 0:
-            id = arg * 1000
-            break
-        case 1:
-            id += arg / 1000
-            break
-        case 2:
-            id = id << (12 + 10) + (arg << 10)
-            break
-        case 3:
-            id += arg
-            break
-        }
-    }
-    return id
+	var id int64
+	for index, arg := range args {
+		switch index {
+		case 0:
+			id = arg * 1000
+			break
+		case 1:
+			id += arg / 1000
+			break
+		case 2:
+			id = id<<(12+10) + (arg << 10)
+			break
+		case 3:
+			id += arg
+			break
+		}
+	}
+	return id
+}
+
+// ParseID method
+func ParseID(id int64) []int64 {
+	miliSecond := id >> 22
+	// 保留中间 12bit
+	sharedID := (id & (0xFFF << 10)) >> 10
+	// 最后 10bit
+	seq := id & 0x3FF
+
+	return []int64{miliSecond, sharedID, seq}
 }
--- main.go
+++ main.go
@@ -1,16 +1,33 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
 
-    IdGen "./idgen"
+	IdGen "./idgen"
 )
 
+var channel chan int = make(chan int)
+
+func loop(idGenerator *IdGen.IDGenerator) {
+	for i := 0; i < 100; i++ {
+		id, err := idGenerator.Next("test", 12345678)
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Printf("%v -> %d \n", IdGen.ParseID(id), id)
+	}
+	channel <- 0
+}
+
 func main() {
-    idGenerator := IdGen.NewIDGenerator("119.27.180.231", 6379, "c5809078fa6d652e0b0232d552a9d06d37fe819c")
-    id, err := idGenerator.Next("test", 12345678)
-    if err != nil {
-        fmt.Println(err)
-    }
-    fmt.Println(id)
+	idGenerator := IdGen.NewIDGenerator("119.27.180.231", 6379, "c5809078fa6d652e0b0232d552a9d06d37fe819c")
+	go loop(idGenerator)
+	go loop(idGenerator)
+	go loop(idGenerator)
+	go loop(idGenerator)
+	go loop(idGenerator)
+	go loop(idGenerator)
+	go loop(idGenerator)
+	go loop(idGenerator)
+	<-channel
 }
